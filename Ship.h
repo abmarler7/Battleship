52a67706aa6417fa85bb7902137094c7da339ac6
abmarler7 <abmarler7@gmail.com>
2013-05-03 10:56:56 -0500

    Update Ship.h

--- Ship.h
+++ Ship.h
@@ -10,7 +10,7 @@
 class Ship
 {
 	protected:
-		int x, y, z, health, damage, length;
+		int x, y, health, damage, length;
 		char direction;
 
 	public:
@@ -20,14 +20,12 @@ class Ship
 		//Get Functions
 		int getX() { return x; };
 		int getY() { return y; };
-		int getZ() { return z; };
 		char getDirection() { return direction; };
 		int getHealth() { return health; };
 		int getLength() { return length; };
 		//Set Functions
 		void setX(int newX) { x = newX; };
 		void setY(int newY) { y = newY; };
-		void setZ(int newZ) { z = newZ; };
 		void setDirection(char newDir) { direction = newDir; };
 		void setHealth(int newHealth) { health = newHealth; };
 		void setLength(int newLength) { length = newLength; };
